9c8def2be341cccabe900468223fd9843c4336ef
lucas <lucas327ks@gmail.com>
2024-02-23 16:06:23 -0300

    add tree `remove()` method

--- Data Structures/Trees/trees-implementation-1.js
+++ Data Structures/Trees/trees-implementation-1.js
@@ -55,21 +55,63 @@ class BinarySearchTree {
 			return false
 		}
 		let currentNode = this.root
+		let parentNode = null
 		while (currentNode) {
 			if (value > currentNode.value) {
+				parentNode = currentNode
 				currentNode = currentNode.right
 			} else if (value < currentNode.value) {
+				parentNode = currentNode
 				currentNode = currentNode.left
-			} else {
-				const holdingPointer = currentNode
-				while (currentNode) {
-					if (currentNode.right) {
-						currentNode = currentNode.right
-					} else {
+			} else if (currentNode.value === value) {
+				// we have a match
+
+				// Case 1: currentNode has no right child
+				if (currentNode.right === null) {
+					if (parentNode === null) {
+						this.root = currentNode.left
+					} else if (currentNode.value > parentNode.value) {
+						parentNode.right = currentNode.left
+					} else if (currentNode.value < parentNode.value) {
+						parentNode.left = currentNode.left
 					}
 				}
+				// Case 2: currentNode has no left child
+				else if (currentNode.right.left === null) {
+					currentNode.right.left = currentNode.left
+					if (parentNode === null) {
+						this.root = currentNode.right
+					} else if (currentNode.value > parentNode.value) {
+						parentNode.right = currentNode.right
+					} else if (currentNode.value < parentNode.value) {
+						parentNode.left = currentNode.right
+					}
+				}
+				// Case 3: Oh boy...
+				else {
+					let leftmost = currentNode.right.left
+					let leftmostParent = currentNode.right
+					while (leftmost.left !== null) {
+						leftmostParent = leftmost
+						leftmost = leftmost.left
+					}
+
+					leftmostParent.left = leftmost.right
+					leftmost.left = currentNode.left
+					leftmost.right = currentNode.right
+
+					if (parentNode !== null) {
+						this.root = leftmost
+					} else if (currentNode.value > parentNode.value) {
+						parentNode.right = leftmost
+					} else if (currentNode.value < parentNode.value) {
+						parentNode.left = leftmost
+					}
+				}
+				return true
 			}
 		}
+		return false
 	}
 }
 
@@ -82,10 +124,10 @@ tree.insert(170)
 tree.insert(15)
 tree.insert(1)
 console.log('Lookup: ', tree.lookup(4))
+tree.remove(20)
+tree.remove(4)
 const print = JSON.stringify(traverse(tree.root))
 console.log(print)
-// tree.remove(170);
-// console.log(tree.lookup(20));
 //     9
 //  4     20
 //1  6  15  170
